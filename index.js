e86b54bf9d7df261121f8217a9e0bd5077234aa1
epsitarif <ariful11233@gmail.com>
2023-05-04 00:17:25 +0600

    added data

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
-const data = require('./data/data.json')
+const allChef = require('./data/data.json');
+const allRecipe = require('./data/recipe.json')
 
 app.get('/', (req, res) =>{
-        res.send('Chef are Rocks')
+        res.json({ message: "hello server is running" })
 });
 
-app.get('/data', (req, res) =>{
-    res.send(data)
+app.get("/allData", (req, res) => {
+    res.send(allChef)
+});
+
+app.get('/data/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = allChef.chef.find(chef => chef._id == id)
+    res.send(selectedChef)
 })
 
-app.listen(port, () => {
-    console.log(`Chef are doing very well: ${port}`)
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipe = allRecipe.filter(recipe => recipe.chef_id == id)
+    res.send(selectedRecipe)
 })
+
+app.listen(5000, () => {
+    console.log("Chef are doing very well")
+});
